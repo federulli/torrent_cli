8cd7cc41bfb460a8c2750241e12ac7258f63e009
Federico Rulli <fede.rulli@hotmail.com>
2016-06-05 18:08:55 -0300

    agrego ventana principal

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,36 +1,29 @@
 from PyQt4 import QtGui, uic
 import os
+from gui.magnet_input_dialog import MagnetInputDialog
+from PyQt4.QtCore import QTimer
 
 headerNames = ["#", "Nombre", "Tamano", "Porcentaje", "Bajada", "Subida", "Seeds", "Peers", "Agregado", "Estado"]
 gui_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ui")
-import re
 
-class MainWindow():
+
+class MainWindow:
     def __init__(self):
         self.__window = uic.loadUi(os.path.join(gui_path, "mainwindow.ui"))
-        self.__dialog = uic.loadUi(os.path.join(gui_path, 'magnetinput.ui'))
-        self.__dialog.buttonBox.buttons()[0].clicked.connect(self.accept)
+        self.__magnet_input_dialog = MagnetInputDialog(self.__window)
         self.__table = self.__window.torrent_table
-        self.__addMagnet = self._MainWindow__window.menuBar.findChild(QtGui.QMenu).actions()[0]
+        self.__addMagnet = self.__window.menuBar.findChild(QtGui.QMenu).actions()[0]
+        self.__addMagnet.triggered.connect(self.__magnet_input_dialog.show)
         self.__build_columns()
-        self.__build_menu()
+        #self._timer = QTimer()
+        #self._timer.timeout.connect(self.timer)
+        #self._timer.start(2)
 
-    def __build_menu(self):
-        self.__addMagnet.triggered.connect(self.on_add_magnet)
 
     def __build_columns(self):
         self.__table.setColumnCount(8)
         self.__table.setHorizontalHeaderLabels(headerNames)
 
-    def on_add_magnet(self):
-        self.__dialog.show()
-
-    def accept(self):
-        magnet_uri = str(self.__dialog.magnetUri.toPlainText())
-        mag = re.sub(r"^magnet.*&dn=([^&]*).*", r"\1", magnet_uri)
-        import urllib
-        mag = urllib.unquote_plus(mag)
-        print mag
 
     def add_row(self, data):
         self.__table.insertRow(self.__table.rowCount())
@@ -51,6 +44,8 @@ class MainWindow():
             self.__table.setItem(index, 6, QtGui.QTableWidgetItem(data["seeds"]))
             self.__table.setItem(index, 7, QtGui.QTableWidgetItem(data["peers"]))
             self.__table.setItem(index, 9, QtGui.QTableWidgetItem(data["state"]))
+
+
 
     def show(self):
         self.__window.show()
